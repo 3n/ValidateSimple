149f584ddac1a2b1c564280f15113d7bd8173f03
3n <ian.collins@gmail.com>
2011-03-04 12:17:46 -0800

    state validator returns matched state

--- Source/ValidateSimple.js
+++ Source/ValidateSimple.js
@@ -304,8 +304,10 @@ ValidateSimple.Validators = {
   },
   'state': {
     test: function(input){
-      var states = ['AL','AK','AS','AZ','AR','AE','AA','AE','AP','CA','CO','CT','DE','DC','FM','FL','GA','GU','HI','ID','IL','IN','IA','KS','KY','LA','ME','MH','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','MP','OH','OK','OR','PW','PA','PR','RI','SC','SD','TN','TX','UT','VT','VI','VA','WA','WV','WI','WY'];
-      return states.contains(input.get('value').clean().toUpperCase());
+      var states = ['AL','AK','AS','AZ','AR','AE','AA','AE','AP','CA','CO','CT','DE','DC','FM','FL','GA','GU','HI','ID','IL','IN','IA','KS','KY','LA','ME','MH','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','MP','OH','OK','OR','PW','PA','PR','RI','SC','SD','TN','TX','UT','VT','VI','VA','WA','WV','WI','WY'],
+          value = input.get('value').clean().toUpperCase();
+      if (states.contains(value))
+        return value;
     }
   }
 };
