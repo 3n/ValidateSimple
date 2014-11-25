10f15aeb39cf42669ee94ba53ac8e7107f8ac8c6
Reed Lauber <reed@simple.com>
2014-11-24 16:57:33 -0800

    Remove .pass(es) from callbacks.

--- Source/ValidateSimple.js
+++ Source/ValidateSimple.js
@@ -94,7 +94,14 @@ var ValidateSimple = new Class({
           if (e.key !== 'tab') this.inputTouched(input);
         }.bind(this));
 
-        var callbacks = [this.validateInput.pass(input, this), this.alertInputValidity.pass(input, this)];
+        var callbacks = [
+          function() {
+            this.validateInput(input);
+          }.bind(this), 
+          function() {
+            this.alertInputValidity(input);
+          }.bind(this)
+        ];
         if (!(Browser.ie8 && input.get('type').test(/checkbox/))) {
           input.addEvent(validateEvent, callbacks[0]);
           if (validateEvent !== 'change') input.addEvent('change', callbacks[0]);
